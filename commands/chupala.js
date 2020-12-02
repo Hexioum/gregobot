20a59e4cf3bb859f1814bc613c8ae12db6d75b1b
Hexioum <36753605+Hexioum@users.noreply.github.com>
2020-12-02 16:54:07 -0300

    Add files via upload

--- commands/chupala.js
+++ commands/chupala.js
@@ -2,7 +2,7 @@
 let replies = ["que te pasa enfermo?", "a veces me pregunto porque dios me sigue dejando vivir en este planeta de mierda", "wn fome", "<:perroHm:739735108314988554>"];
 module.exports = {
 	name: 'chupala',
-	aliases: ['chupalo','haceme un kiko','chupame el pico','chupa el pene', 'mascala', 'mascalo'],
+	aliases: ['chupalo','mamala','mamalo','haceme un kiko','chupame el pico','chupa el pene','mascala','mascalo','agachate y conocelo'],
 	description: 'Gregorio se defiende',
 	execute(message) {
 	let random = Math.floor(Math.random() * 4);
