615e299953a686fed7453b824ddb2bdb78d31b72
Hexioum <36753605+Hexioum@users.noreply.github.com>
2020-12-16 20:00:29 -0300

    Add files via upload

--- commands/saluda.js
+++ commands/saluda.js
@@ -1,10 +1,11 @@
 // Array list of replies
-let replies = ["wena wn", "buen dia grupo <:grBliss:749880374213083227>", "<:Grego:750502194108956682>:interrobang:"];
+let replies = ["wena wn", "buen dia grupo <:grBliss:749880374213083227>", "<:Grego:750502194108956682>:interrobang:", "sry manito ando subiendo la montaña"];
 module.exports = {
 	name: 'saluda',
+	aliases: ['salute','salude','saludame','salúdame'],
 	description: 'Gregorio te saluda',
 	execute(message) {
-	let random = Math.floor(Math.random() * 3);
+	let random = Math.floor(Math.random() * 4);
 		message.channel.send(replies[random]);
 	},
 };
