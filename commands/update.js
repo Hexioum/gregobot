27c332e1dd57829bdcbf31ac32d6f4dbf7c85d0a
Hexioum <36753605+Hexioum@users.noreply.github.com>
2020-12-29 21:54:21 -0300

    Add files via upload

---
+++ commands/update.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'update',
+	aliases: ['actualiza el avatar','arregla el avatar','cambia el avatar','cambia la foto'],
+	description: 'Gregorio cambia su avatar.',
+	execute(message) {
+	client.users.fetch(gregorid).then(myUser => {
+    client.user.setAvatar(myUser.avatarURL())})
+	.then(() => console.log(myUser))
+	.catch(() => console.log(`Cambié el avatar hace muy poco...`));
+	},
+};
