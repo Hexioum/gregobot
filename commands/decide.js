6f94ce84450d5bcdd62e1510bf9a0cb3755bb4bc
Hexioum <36753605+Hexioum@users.noreply.github.com>
2021-03-16 13:57:24 -0300

    Add files via upload

--- commands/decide.js
+++ commands/decide.js
@@ -6,8 +6,10 @@ module.exports = {
 	usage: '<opcion1>, <opcion2>',
 	execute(message, args) {
 	message.channel.startTyping();
-	var theRegex = / gay+| lgbt+/gi ;
-	var oneMatch = args.some(e => theRegex.test(e));
+	var oneRegex = / gay+| lgbt+/gi ;
+	var oneMatch = args.some(e => oneRegex.test(e));
+	var twoRegex = / peruano+| venezolano+| venaco+| negro+| haitiano+/gi ;
+	var twoMatch = args.some(e => twoRegex.test(e));
 setTimeout(function(){
 	let pos = args.indexOf("o")
 	if (pos >= 0)
@@ -20,7 +22,6 @@ setTimeout(function(){
 		args.splice(pos3, 1);
 	let random = Math.floor(Math.random()*args.length)
 		console.log(args)
-		console.log(oneMatch)
 		console.log(`Se le han proporcionado a Grego ${args.length} opciones y eligió la número ${random}`)
 		if (args.length < 2) {
 			message.channel.send('faltan opciones');
@@ -72,7 +73,12 @@ setTimeout(function(){
 			message.channel.send('que asco 😦')
 			.catch(() => console.error('Que onda?? No pude responder.'));
 		}
-		else message.channel.send(args[random])
+		else if (twoMatch === true) {
+			message.channel.send('antes muerto')
+			.catch(() => console.error('Que onda?? No pude responder.'));
+		}
+		//	Removes special symbols.
+		else message.channel.send(args[random].replace(/[^\w\s\xc0-\xff]/gi, ''))
 		return message.channel.stopTyping(true);
 	}, 250);
 }};
