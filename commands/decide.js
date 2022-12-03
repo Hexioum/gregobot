e2baa75e3338f4ad58516a3c766f2429ff12ed40
hexioum <lfminet@live.cl>
2022-12-03 18:48:04 -0300

    Cleaner code.

--- commands/decide.js
+++ commands/decide.js
@@ -12,78 +12,79 @@ module.exports = {
 	var twoMatch = args.some(e => twoRegex.test(e));
 	var trdRegex = / furro+| furry+| foorry+| fursuit+| zoofili+| zoofíli+/gi ;
 	var trdMatch = args.some(e => trdRegex.test(e));
-setTimeout(function(){
-	let pos = args.indexOf("o")
-	if (pos >= 0)
-		args.splice(pos, 1);
-	let pos2 = args.indexOf("entre")
-	if (pos2 >= 0)
-		args.splice(pos2, 1);
-	let pos3 = args.indexOf("gear")
-	if (pos3 >= 0)
-		args.splice(pos3, 1);
-	let random = Math.floor(Math.random()*args.length)
-		console.log(args)
-		console.log(`Se le han proporcionado a Grego ${args.length} opciones y eligió la número ${random}`)
-		if (args.length < 2) {
-			message.reply('faltan opciones');
-		}
-		else if (args[0] === 'o') {
-			message.reply('!?!?!?!?!?');
-		}
-		else if (trdMatch === true) {
-			message.reply('por ultima vez... SOY YIFFER')
-			.catch(() => console.error('Que onda?? No pude responder.'));
-		}
-		else if (args[random] === 'o') {
-			message.reply(`${args[0]}`);
-		}
-		else if (args[random] === 'ki') {
-			message.reply('killer es');
-		}
-		else if (args[random] === 'mbaa') {
-			message.reply('melty es');
-		}
-		else if (args[random] === 'guilty gear') {
-			message.reply('guilty es');
-		}
-		else if (args[random].toLowerCase() === 'ser gay') {
-			message.reply({files: ['./memes/grillos.png']})
-			.catch(() => console.error('Que onda?? No pude responder.'));
-		}
-		else if (args[random].toLowerCase() === 'ser negro') {
-			message.reply('no sabria decirte')
-			.catch(() => console.error('Que onda?? No pude responder.'));
-		}
-		else if (args[random].toLowerCase() === 'pico') {
-			message.reply({files: ['./memes/grillos.png']})
-			.catch(() => console.error('Que onda?? No pude responder.'));
-		}
-		else if (args[random].toLowerCase() === 'pene') {
-			message.reply({files: ['./memes/grillos.png']})
-			.catch(() => console.error('Que onda?? No pude responder.'));
-		}
-		else if (args[random].toLowerCase() === 'tula') {
-			message.reply({files: ['./memes/grillos.png']})
-			.catch(() => console.error('Que onda?? No pude responder.'));
-		}
-		else if (args[random].toLowerCase() === 'pichula') {
-			message.reply({files: ['./memes/grillos.png']})
-			.catch(() => console.error('Que onda?? No pude responder.'));
-		}
-		else if (args[random] === 'patas') {
-			message.reply({files: ['./memes/momopatas.png']})
-			.catch(() => console.error('Que onda?? No pude responder.'));
-		}
-		else if (twoMatch === true) {
-			message.reply('mejor morir')
-			.catch(() => console.error('Que onda?? No pude responder.'));
-		}
-		else if (oneMatch === true) {
-			message.reply('que asco 😦')
-			.catch(() => console.error('Que onda?? No pude responder.'));
-		}
-		//	Removes special symbols.
-		else message.reply(args[random].replace(/[^\w\s\xc0-\xff]/gi, ''))
-	}, 2000);
-}};
+	setTimeout(function(){
+		let pos = args.indexOf("o")
+		if (pos >= 0)
+			args.splice(pos, 1);
+		let pos2 = args.indexOf("entre")
+		if (pos2 >= 0)
+			args.splice(pos2, 1);
+		let pos3 = args.indexOf("gear")
+		if (pos3 >= 0)
+			args.splice(pos3, 1);
+		let random = Math.floor(Math.random()*args.length)
+			console.log(args)
+			console.log(`Se le han proporcionado a Grego ${args.length} opciones y eligió la número ${random}`)
+			if (args.length < 2) {
+				message.reply('faltan opciones');
+			}
+			else if (args[0] === 'o') {
+				message.reply('!?!?!?!?!?');
+			}
+			else if (trdMatch === true) {
+				message.reply('por ultima vez... SOY YIFFER')
+				.catch(() => console.error('Que onda?? No pude responder.'));
+			}
+			else if (args[random] === 'o') {
+				message.reply(`${args[0]}`);
+			}
+			else if (args[random] === 'ki') {
+				message.reply('killer es');
+			}
+			else if (args[random] === 'mbaa') {
+				message.reply('melty es');
+			}
+			else if (args[random] === 'guilty gear') {
+				message.reply('guilty es');
+			}
+			else if (args[random].toLowerCase() === 'ser gay') {
+				message.reply({files: ['./memes/grillos.png']})
+				.catch(() => console.error('Que onda?? No pude responder.'));
+			}
+			else if (args[random].toLowerCase() === 'ser negro') {
+				message.reply('no sabria decirte')
+				.catch(() => console.error('Que onda?? No pude responder.'));
+			}
+			else if (args[random].toLowerCase() === 'pico') {
+				message.reply({files: ['./memes/grillos.png']})
+				.catch(() => console.error('Que onda?? No pude responder.'));
+			}
+			else if (args[random].toLowerCase() === 'pene') {
+				message.reply({files: ['./memes/grillos.png']})
+				.catch(() => console.error('Que onda?? No pude responder.'));
+			}
+			else if (args[random].toLowerCase() === 'tula') {
+				message.reply({files: ['./memes/grillos.png']})
+				.catch(() => console.error('Que onda?? No pude responder.'));
+			}
+			else if (args[random].toLowerCase() === 'pichula') {
+				message.reply({files: ['./memes/grillos.png']})
+				.catch(() => console.error('Que onda?? No pude responder.'));
+			}
+			else if (args[random] === 'patas') {
+				message.reply({files: ['./memes/momopatas.png']})
+				.catch(() => console.error('Que onda?? No pude responder.'));
+			}
+			else if (twoMatch === true) {
+				message.reply('mejor morir')
+				.catch(() => console.error('Que onda?? No pude responder.'));
+			}
+			else if (oneMatch === true) {
+				message.reply('que asco 😦')
+				.catch(() => console.error('Que onda?? No pude responder.'));
+			}
+			//	Removes special symbols.
+			else message.reply(args[random].replace(/[^\w\s\xc0-\xff]/gi, ''))
+		}, 2000);
+	}
+};
