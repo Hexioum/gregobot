508e7f4cb6d5d593c415dfa4bb7c42e2b179c11a
hexioum <lfminet@live.cl>
2022-12-03 22:32:31 -0300

    Prevent crashes when message has a tag.

--- commands/sonicdice.js
+++ commands/sonicdice.js
@@ -4,7 +4,7 @@ const wrapText = require('wrap-text');
 
 module.exports = {
 	name: 'sonicdice',
-	aliases: ['sd','sonicdice','sonicsays'],
+	aliases: ['test','sonicdice','sonicsays'],
 	description: 'Genera un mensaje inspiracional.',
 	args: true,
 	usage: 'Debe especificar mensaje.',
@@ -39,7 +39,19 @@ module.exports = {
           }
         };
 
-        function evaluate(args) {
+        async function evaluate(args) {
+          /*var mentions = args.match(/<.*>/g);
+          if (mentions!==null) {
+            for (var i = 0; i < mentions.length; i++) {
+              if (mentions[i].startsWith('<@&')) {
+                mentions[i] = await message.guild.roles.cache.find(mentions[i]);
+              } else {
+                mentions[i] = await message.guild.members.cache.find(mentions[i]);
+              }
+              args = args.replace(/<.*>/, mentions[i]);
+            }
+          };*/
+          args = args.replace(/<.*>/g, '');
           var argWidth = 12+(Math.floor(args.length*0.07));
           if (argWidth > 38) {
             argWidth = 38;
@@ -47,72 +59,83 @@ module.exports = {
           if (args.length > 435) {
               message.reply('XD');
           } else {
-              // FIX: Long words from NOT wrapping.
               var arr = wrapText(args, argWidth);
               var topOffset = 0;
-              arr = wrap(arr, topOffset);
+              arr = wrap(arr, topOffset, argWidth);
               drawImage(arr, topOffset);
           };
         }
 
-        function wrap(arr, topOffset) {
-            // Escape certain characters. Prevents crashes.
-            arr = arr.charAt(0).toUpperCase() + arr.slice(1);
-            arr = arr.replace(/\*{2}/gi, ``);//Only two *s.
-            arr = arr.replace(/"+/gi, `&quot;`);
-            arr = arr.replace(/<+/gi, `&lt;`);
-            arr = arr.replace(/>+/gi, `&gt;`);
-            arr = arr.replace(/\n\s*\n/g, '\n');
-            arr = arr.split('\n');
-            var size = (14/Math.sqrt(args.length))+1;
-            var font = "Arial";
-            if (arr.length == 1) {
-              var offset = 13;
-            } else if (arr.length == 2) {
-              var offset = -14;
-            } else if (arr.length == 3) {
-              var offset = -36;
-            } else if (arr.length == 4) {
-              var offset = -60;
-            } else if (arr.length == 5) {
-              var offset = -66;
-            } else if (arr.length == 6) {
-              var offset = -80;
-            } else if (arr.length == 7) {
-              var offset = -86;
-            } else if (arr.length > 11) {
-              var offset = (arr.length-1)*-11;
-            } else {
-              var offset = (arr.length-1)*-12;
-            }
-            //(arr.length-1)*3;//2 lineas debe ser 3, y 11 lineas 33, pero hay que hacer que la curva decrezca
-            for (var i = 0; i < arr.length; i++) {
-              arr[i] = `
-              <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.045em" dy="${topOffset+.295}em" fill="#003B79" font-family="${font}" font-size="${size}em">${arr[i]}</text>
-              <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.04em" dy="${topOffset+.29}em" fill="#001B36" font-family="${font}" font-size="${size}em">${arr[i]}</text>
-              <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.035em" dy="${topOffset+.285}em" fill="#001224" font-family="${font}" font-size="${size}em">${arr[i]}</text>
-              <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.03em" dy="${topOffset+.28}em" fill="#000912" font-family="${font}" font-size="${size}em">${arr[i]}</text>
-              <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.025em" dy="${topOffset+.275}em" fill="#000304" font-family="${font}" font-size="${size}em">${arr[i]}</text>
-              <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.02em" dy="${topOffset+.27}em" fill="#000" font-family="${font}" font-size="${size}em">${arr[i]}</text>
-              <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.015em" dy="${topOffset+.265}em" fill="#000" font-family="${font}" font-size="${size}em">${arr[i]}</text>
-              <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.01em" dy="${topOffset+.26}em" fill="#000" font-family="${font}" font-size="${size}em">${arr[i]}</text>
-              <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dy="${topOffset+.25}em" fill="#fff" font-family="${font}" font-size="${size}em">${arr[i]}</text>
-              `
-              if (i == 0) {
-                topOffset = 0;
-              }
-              if (arr.length > 8) {
-                topOffset = topOffset+(size*0.7);
-              } else if (arr.length > 5) {
-                topOffset = topOffset+(size*0.5);
-              } else if (arr.length > 4) {
-                topOffset = topOffset+(size*0.425);
+        function wrap(arr, topOffset, argWidth) {
+          // Escape certain characters. Prevents crashes.
+          arr = arr.charAt(0).toUpperCase() + arr.slice(1);
+          arr = arr.replace(/\*{2}/gi, ``);//Only two *s.
+          arr = arr.replace(/"+/gi, `&quot;`);
+          arr = arr.replace(/<+/gi, `&lt;`);
+          arr = arr.replace(/>+/gi, `&gt;`);
+          arr = arr.replace(/\n\s*\n/g, '\n');
+          arr = arr.split('\n');
+          for (var i = 0; i < arr.length; i++) {
+            // Wraps very long words.
+            // Problem: arr.length will change if line is added, but it will stop iterating.
+            if (arr[i].length-1 > argWidth) {
+              let slice = arr[i].slice(arr[i].length-1-argWidth, arr[i].length);
+              arr[i] = arr[i].slice(0, argWidth);
+              if (typeof arr[i+1] === 'undefined') {
+                arr.push(slice);
               } else {
-                topOffset = topOffset+(size*arr.length*0.125);
-              };
+                arr.splice(i+1, 0, slice);
+              }
             }
-            return arr;
+          };
+          var size = (14/Math.sqrt(args.length))+1;
+          var font = "Arial";
+          if (arr.length == 1) {
+            var offset = 13;
+          } else if (arr.length == 2) {
+            var offset = -14;
+          } else if (arr.length == 3) {
+            var offset = -36;
+          } else if (arr.length == 4) {
+            var offset = -60;
+          } else if (arr.length == 5) {
+            var offset = -66;
+          } else if (arr.length == 6) {
+            var offset = -80;
+          } else if (arr.length == 7) {
+            var offset = -86;
+          } else if (arr.length > 11) {
+            var offset = (arr.length-1)*-11;
+          } else {
+            var offset = (arr.length-1)*-12;
           }
+          for (var i = 0; i < arr.length; i++) {
+            arr[i] = `
+            <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.045em" dy="${topOffset+.295}em" fill="#003B79" font-family="${font}" font-size="${size}em">${arr[i]}</text>
+            <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.04em" dy="${topOffset+.29}em" fill="#001B36" font-family="${font}" font-size="${size}em">${arr[i]}</text>
+            <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.035em" dy="${topOffset+.285}em" fill="#001224" font-family="${font}" font-size="${size}em">${arr[i]}</text>
+            <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.03em" dy="${topOffset+.28}em" fill="#000912" font-family="${font}" font-size="${size}em">${arr[i]}</text>
+            <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.025em" dy="${topOffset+.275}em" fill="#000304" font-family="${font}" font-size="${size}em">${arr[i]}</text>
+            <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.02em" dy="${topOffset+.27}em" fill="#000" font-family="${font}" font-size="${size}em">${arr[i]}</text>
+            <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.015em" dy="${topOffset+.265}em" fill="#000" font-family="${font}" font-size="${size}em">${arr[i]}</text>
+            <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dx="0.01em" dy="${topOffset+.26}em" fill="#000" font-family="${font}" font-size="${size}em">${arr[i]}</text>
+            <text x="81%" y="50%" transform="translate(0, ${offset})" text-anchor="middle" dy="${topOffset+.25}em" fill="#fff" font-family="${font}" font-size="${size}em">${arr[i]}</text>
+            `
+            if (i == 0) {
+              topOffset = 0;
+            }
+            if (arr.length > 8) {
+              topOffset = topOffset+(size*0.7);
+            } else if (arr.length > 5) {
+              topOffset = topOffset+(size*0.5);
+            } else if (arr.length > 4) {
+              topOffset = topOffset+(size*0.425);
+            } else {
+              topOffset = topOffset+(size*arr.length*0.125);
+            };
+          }
+          return arr;
+        }
 
         function drawImage(arr, topOffset) {
           console.log(`Generando imagen con texto: ${args}`);
